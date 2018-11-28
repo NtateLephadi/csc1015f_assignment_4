cb3cb106ee43626ab5456c088dcc9e54d297c839
Tumelo Lephadi <lphtum003@myuct.ac.za>
2018-11-28 21:38:59 +0200

    to_english is done

--- piglatin.py
+++ piglatin.py
@@ -1,9 +1,10 @@
+# return the Pig Latin sentence for a given English sentence.
 def to_pig_latin(sentence):
     pig_latin = ''
     sentence = sentence.split()
     for word in sentence:
         if word[0] in ['a', 'e', 'i', 'o', 'u']:
-            word = word + 'way '
+            word = word + 'way'
         else:
             consonant = ''
             for i in word:
@@ -11,6 +12,29 @@ def to_pig_latin(sentence):
                     break
                 else:
                     consonant += i
-            word = word[len(consonant)::] + consonant + 'ay '
-        pig_latin += word
+            word = word[len(consonant)::] + 'a' + consonant + 'ay'
+        pig_latin += word + ' '
     return pig_latin
+
+def remove_way(word):
+    word = word[:len(word) - 3]
+    return word
+
+def remove_ay(word):
+    word = word[:len(word) - 2]
+    return word
+
+# return the English sentence for a given Pig Latin sentence.
+def to_english(sentence):
+    english = ''
+    sentence = sentence.split()
+    for word in sentence:
+        if word[len(word) - 3::] == 'way':
+            word = remove_way(word)
+        else:
+            word = remove_ay(word)
+            consonants = word[word.rindex('a') + 1:]
+            word = word[:word.rindex('a')]
+            word = consonants + word
+        english += word + ' '
+    return english
